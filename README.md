24f725c6664b90cdfeeeb833a6f87ffcb3e313d9
Ka9ddc <ka9ddcgamer@gmail.com>
2025-05-11 18:32:25 -0300

    feat: galton board

--- README.md
+++ README.md
@@ -1,17 +1,26 @@
 
 # Projetos de Sistemas Embarcados - EmbarcaTech 2025
 
-Autor: **Insira Seu Nome**
+Autor: **Ryan Micael Benício dos Santos**
 
 Curso: Residência Tecnológica em Sistemas Embarcados
 
 Instituição: EmbarcaTech - HBr
 
-Campinas, ___ de 2025
+Brasília, Maio de 2025
 
 ---
 
 <!-- INSIRA O CONTEÚDO DO SEU README AQUI! -->
+
+## Projeto LED Embutido Modular
+
+Este projeto simula uma **Galton Board**, utilizando o display OLED via I2C presente na BitDogLab. A simulação exibe bolas "caindo" por fileiras de pinos, desviando aleatoriamente à esquerda ou direita, até serem registradas em um histograma que representa a distribuição das quedas.
+
+---
+### Como compilar?
+
+Para compilar basta usar o CMake no diretório principal.
 
 ---
 
--- src/lab-01-galton-board.c
+++ src/lab-01-galton-board.c
@@ -5,25 +5,36 @@
 // Biblioteca para uso do SSD1306, display OLED.
 #include "include/ssd1306.h"
 
+// Bibliotecas da modularização
+#include "include/galton.h"
+#include "include/hal_i2c.h"
+#include "include/display.h"
+
 // Definições para uso de comunicação I2C com os pinos do display OLED.
 #define I2C_PORT i2c0
 #define I2C_SDA 14
 #define I2C_SCL 15
 
-int main()
-{
-    stdio_init_all();
+ssd1306_t display;
 
-    // Inicialização da porta I2C em 400Khz.
-    i2c_init(I2C_PORT, 400*1000);
-    
-    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
-    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_SDA);
-    gpio_pull_up(I2C_SCL);
+void init_i2c_display() {
+
+    hal_i2c_init();
+    ssd1306_init(&display, DISPLAY_WIDTH, DISPLAY_HEIGHT, 0x3C, i2c1);;
+    ssd1306_clear(&display);
+    ssd1306_show(&display);
+
+}
+
+int main() {
+    stdio_init_all();
+    init_i2c_display();
 
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        galton_tick();
+        display_render();
+        sleep_ms(50);
     }
+
+    return 0;
 }
